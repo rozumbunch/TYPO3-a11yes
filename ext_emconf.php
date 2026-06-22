d51491759267103f14fcac029caaeda416581114
Boris Andrachnik <b.andrachnik@ev-medienhaus.de>
2026-06-22 22:29:45 +0200

    add depends for v14

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.4.0-14.0.99',
+            'typo3' => '13.4.0-14.3.99',
         ],
     ],
     'autoload' => [
